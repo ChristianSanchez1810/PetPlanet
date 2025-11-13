015036a231f9bca306bf2002bf85de88dde7760c
ChristianSanchez1810 <ariel1810mytroo@gmail.com>
2025-11-13 01:13:41 -0600

    carrusel con fotos

---
+++ frontend/assets/js/main.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContentLoaded", () => {
+    
+    const slides = document.querySelectorAll(".slide");
+    const dots = document.querySelectorAll(".dot");
+    const prevBtn = document.querySelector(".prev");
+    const nextBtn = document.querySelector(".next");
+
+    
+    let indiceActivo = 0;
+
+    function mostrarSlide(indice) {
+        slides[indiceActivo].classList.remove("activa");
+        dots[indiceActivo].classList.remove("activo");
+        indiceActivo = indice;
+        slides[indiceActivo].classList.add("activa");
+        dots[indiceActivo].classList.add("activo");
+    }
+
+    function siguienteSlide() {
+        let nuevoIndice = (indiceActivo + 1) % slides.length;
+        mostrarSlide(nuevoIndice);
+    }
+
+    function anteriorSlide() {
+        let nuevoIndice = (indiceActivo - 1 + slides.length) % slides.length;
+        mostrarSlide(nuevoIndice);
+    }
+    
+    nextBtn.addEventListener("click", () => {
+        siguienteSlide();
+    });
+
+    prevBtn.addEventListener("click", () => {
+        anteriorSlide();
+    });
+
+    dots.forEach((dot, indice) => {
+        dot.addEventListener("click", () => {
+            mostrarSlide(indice);
+        });
+    });
+    
+    setInterval(() => {
+        siguienteSlide();
+    }, 5000); 
+
+});
